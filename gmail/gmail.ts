70f6960d048d8532c278a7ee8cfed75ca7204bb5
Zweer <n.olivieriachille@gmail.com>
2024-12-23 11:48:45 +0100

    perf: :bug: Fixed some exist conditions

--- gmail/gmail.ts
+++ gmail/gmail.ts
@@ -1,42 +1,76 @@
 const subjectRegex = /(.*) - (.*)/;
+const mangaMailerSender = 'MangaMailer <mailtrap@demomailtrap.com>';
 
-function mangaChapters(event) {
+interface Event {
+  messageMetadata: {
+    accessToken: string;
+    messageId: string;
+  };
+}
+
+export function mangaChapters(event: Event) {
   const { accessToken, messageId } = event.messageMetadata;
 
   GmailApp.setCurrentMessageAccessToken(accessToken);
 
+  let title: string;
+  let section;
+
   const message = GmailApp.getMessageById(messageId);
+
+  const sender = message.getFrom();
+  if (sender !== mangaMailerSender) {
+    const card = CardService.newCardBuilder()
+      .setHeader(CardService.newCardHeader().setTitle('Wrong message'))
+      .addSection(
+        CardService.newCardSection().addWidget(
+          CardService.newDecoratedText().setText('This message is from Manga Mailer'),
+        ),
+      )
+      .build();
+
+    return [card];
+  }
+
   const subject = message.getSubject();
-  const [, title] = subjectRegex.exec(subject);
+  const match = subjectRegex.exec(subject);
+  if (match) {
+    [, title] = match;
 
-  const messages = GmailApp.search(title).sort((messageA, messageB) => {
-    const [, , chapterStringA] = subjectRegex.exec(messageA.getFirstMessageSubject());
-    const chapterA = parseInt(chapterStringA, 10);
-    const [, , chapterStringB] = subjectRegex.exec(messageB.getFirstMessageSubject());
-    const chapterB = parseInt(chapterStringB, 10);
+    const messages = GmailApp.search(title)
+      .filter((message) => subjectRegex.exec(message.getFirstMessageSubject()))
+      .sort((messageA, messageB) => {
+        const [, , chapterStringA] = subjectRegex.exec(messageA.getFirstMessageSubject())!;
+        const chapterA = parseInt(chapterStringA, 10);
+        const [, , chapterStringB] = subjectRegex.exec(messageB.getFirstMessageSubject())!;
+        const chapterB = parseInt(chapterStringB, 10);
 
-    return chapterB - chapterA;
-  });
+        return chapterB - chapterA;
+      });
 
-  const section = CardService.newCardSection().setHeader(
-    '<font color="#1257e0"><b>Previous Chapters</b></font>',
-  );
+    section = CardService.newCardSection().setHeader(
+      '<font color="#1257e0"><b>Previous Chapters</b></font>',
+    );
 
-  for (const message of messages) {
-    const [, , messageChapterString] = /(.*) - (.*)/.exec(message.getFirstMessageSubject());
-    const messageLink = `https://mail.google.com/mail/u/0/#inbox/${message.getId()}`;
+    for (const message of messages) {
+      const [, , messageChapterString] = /(.*) - (.*)/.exec(message.getFirstMessageSubject())!;
+      const messageLink = `https://mail.google.com/mail/u/0/#inbox/${message.getId()}`;
 
-    Logger.log(`chapter ${messageChapterString} isInInbox ${message.isInInbox()}`);
+      const link = CardService.newDecoratedText()
+        .setText(messageChapterString)
+        .setOpenLink(CardService.newOpenLink().setUrl(messageLink));
 
-    const link = CardService.newDecoratedText()
-      .setText(messageChapterString)
-      .setOpenLink(CardService.newOpenLink().setUrl(messageLink));
+      if (message.isInInbox()) {
+        link.setStartIcon(CardService.newIconImage().setIcon(CardService.Icon.EMAIL));
+      }
 
-    if (message.isInInbox()) {
-      link.setStartIcon(CardService.newIconImage().setIcon(CardService.Icon.EMAIL));
+      section.addWidget(link);
     }
-
-    section.addWidget(link);
+  } else {
+    title = 'Something bad happened';
+    section = CardService.newCardSection().addWidget(
+      CardService.newDecoratedText().setText(`Error while processing chapter "${subject}"`),
+    );
   }
 
   const card = CardService.newCardBuilder()
