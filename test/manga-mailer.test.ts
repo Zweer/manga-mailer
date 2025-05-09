5aeccb5ecb5fa651fb2d427ca6a14d4cc6582fa9
Zweer <n.olivieriachille@gmail.com>
2025-05-09 15:47:07 +0200

    feat: :sparkles: `aws-cdk` scaffolding

---
+++ test/manga-mailer.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MangaMailer from '../lib/manga-mailer-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/manga-mailer-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MangaMailer.MangaMailerStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
