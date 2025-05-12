6892dd811911453eae3804ef8138c21ebf6e3f2d
Zweer <n.olivieriachille@gmail.com>
2025-05-12 14:40:52 +0200

    refactor: :recycle: Restarting from start

--- lib/constants.ts
+++
@@ -1,4 +0,0 @@
-export const PROJECT_NAME = 'manga-mailer';
-
-export const sessionKey = 'key';
-export const userKey = 'userId';
--- src/manga/interfaces/manga.ts
+++
@@ -1,9 +0,0 @@
-import type { ConnectorNames } from '@zweer/manga-scraper';
-
-export interface Manga {
-  site: ConnectorNames;
-  id: string;
-  title?: string;
-  lastReadChapter: number;
-  disabled?: boolean;
-}
