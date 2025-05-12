0d3a21691a7b2d688dd162da462eaa8403a73f62
Zweer <n.olivieriachille@gmail.com>
2025-05-12 16:51:55 +0200

    feat(bot): :sparkles: Added the webhook info as GET

--- app/route.ts
+++ app/route.ts
@@ -1,7 +1,24 @@
+/* eslint-disable ts/no-unsafe-member-access */
+/* eslint-disable ts/no-unsafe-call */
+/* eslint-disable ts/no-unsafe-assignment */
 import { webhookCallback } from 'grammy';
+import { NextResponse } from 'next/server';
 
 import { createBot } from '@/lib/bot';
 
 const bot = createBot();
 
 export const POST = webhookCallback(bot, 'std/http');
+
+export async function GET() {
+  try {
+    const webhook = await bot.api.getWebhookInfo();
+
+    return NextResponse.json({ webhook });
+  } catch (error) {
+    console.error('Error while retrieving webhook');
+    console.info(error);
+
+    return NextResponse.json({ error });
+  }
+}
