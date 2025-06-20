c90c6068672273e6a721a71b834a8000617242f6
Zweer <n.olivieriachille@gmail.com>
2025-06-20 22:58:55 +0200

    perf(infra): :label: Added the tags

--- lib/stack/database-stack.ts
+++ lib/stack/database-stack.ts
@@ -5,12 +5,15 @@ import { NestedStack } from 'aws-cdk-lib';
 import { AttributeType, TableV2 } from 'aws-cdk-lib/aws-dynamodb';
 
 import { PROJECT_NAME } from '../constants.js';
+import { tagMe } from '../utils.js';
 
 export class DatabaseStack extends NestedStack {
   userTable: TableV2;
 
   constructor(scope: Construct, id: string, props?: NestedStackProps) {
     super(scope, id, props);
+
+    tagMe(this, 'database');
 
     this.userTable = new TableV2(this, 'UserTable', {
       tableName: `${PROJECT_NAME}-user`,
