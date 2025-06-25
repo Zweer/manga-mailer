a4200ad2dd14b94f9f6f93a32b5ae6f0e4f5d589
Zweer <n.olivieriachille@gmail.com>
2025-06-25 16:59:36 +0200

    feat: :technologist: Added the QoL dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import antfu from '@antfu/eslint-config';
+import nextPlugin from '@next/eslint-plugin-next';
+
+export default antfu({
+  react: true,
+  stylistic: {
+    semi: true,
+    overrides: {
+      'style/brace-style': ['error', '1tbs'],
+      'no-console': 'off',
+    },
+  },
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
+  },
+}, {
+  plugins: {
+    '@next/next': nextPlugin,
+  },
+}, {
+  rules: {
+    'node/prefer-global/process': 'off',
+    'perfectionist/sort-imports': ['error', {
+      internalPattern: ['^~/.+', '^@/.+', '^#.+'],
+      groups: [
+        'type',
+        ['parent-type', 'sibling-type', 'index-type', 'internal-type'],
+        'builtin',
+        'external',
+        'internal',
+        ['parent', 'sibling', 'index'],
+        'side-effect',
+        'object',
+        'unknown',
+      ],
+    }],
+  },
+}, {
+  ignores: ['.next/*'],
+});
